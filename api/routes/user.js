70fe21d08d59399c23e807b9bf29155efd68714d
tylerpitcher <tylerpitch2015@gmail.com>
2022-01-17 16:44:42 -0500

    Add user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,4 +2,25 @@ const express = require('express');
 const db = require('../db/database');
 const router = express.Router();
 
+router.post('/register', async (req, res) => {
+    const user = await db.createUser();
+    res.status(200).json({
+        success: user.key != undefined,
+        key: user.key
+    });
+});
+
+router.get('/get/:key', async (req, res) => {
+    let user = await db.getUserByKey(req.params.key);
+    res.status(200).json({ 
+        success: user != undefined, 
+        user: user 
+    });
+});
+
+router.delete('/delete', async (req, res) => {
+    let result = await db.deleteUser(req.body.key);
+    res.status(200).json({ success: result == 1 });
+});
+
 module.exports = router;
